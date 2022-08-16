23a34d07b3757c48fcb90c7d6c54903069ec7dcf
omesh valyal <omeshvalyal@gmail.com>
2022-08-16 16:28:24 +0530

    credential ids changed

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,6 +1,6 @@
 def checkout_git() {
     
     echo "This is for checking out git code."
-   git credentialsId: '95cb58f4-0104-4704-9cb9-9b46e3faf8ef', url: 'git@github.com:omeshvalyal/java-hello-world-with-maven.git'    
+   git credentialsId: '17390d8a-0226-47a9-b764-812df56ad510', url: 'git@github.com:omeshvalyal/maven-hello-world.git'    
 }
 return this
